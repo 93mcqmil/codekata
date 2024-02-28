9635921b4222aa9eea7d58b1bcfc711805563305
93mcqmil <mcqueen_1993@hotmail.com>
2024-02-28 13:36:41 +0100

    made a h1 tagg

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Header from "./Components/Header/Header";
+
+const App = () => {
+  return (
+    <>
+      <p>liten-kod-kata-react</p>
+      <Header headerText='Steg 4 klart' />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,6 @@
+import "./Header.css";
+
+const Header = ({ headerText }) => {
+  return <h1>liten-kod-kata-react</h1>;
+};
+export default Header;
